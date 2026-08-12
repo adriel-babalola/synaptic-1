cadf67542fd3ecea51381631c9f0f0e7097fabc9
adriel-babalola <babalolaaauge115@st.futminna.edu.ng>
2026-08-12 22:40:36 +0100

    minor targeted search query and better folder trcture

--- stage-1/file-handling/experiment/working_on_csv.py
+++ stage-1/file-handling/experiment/working_on_csv.py
@@ -4,13 +4,17 @@ import os
 project_dir = os.path.dirname(__file__)
 data_path = os.path.join(project_dir, "output", "info.csv")
 
+title=["Name","Country", "Email"]
+
 data_to_append = [
+    title,
     ["janet", "USA", "her@gmail.com"],
     ["dude", "Nigeria", "dude@gmail.com"]
 ]
 
 file = open(data_path, "a", newline="")
 writer = csv.writer(file)
+
 writer.writerows(data_to_append)
 
-file.close()
+file.close() 
--- stage-1/file-handling/info_fetch.py
+++ stage-1/file-handling/info_fetch.py
@@ -1,11 +1,13 @@
 import os
 import csv
 from urllib.request import urlopen
+# Intsall biopython and dotenv to use
 from Bio import Entrez
 from dotenv import load_dotenv
 
 load_dotenv()
 
+# getting the porj directory, and creating my save path and save name
 project_dir = os.path.dirname(__file__)
 data_path = os.path.join(project_dir, "output", "results.csv")
 
@@ -16,13 +18,21 @@ Entrez.api_key = os.getenv("ENTREZ_API_KEY")
 Entrez.email = os.getenv("ENTREZ_EMAIL")
 Entrez.tool = "synaptic-1"
 
+
+# Serach Query
+query="""
+PfDHFR[Title/Abstract] AND (inhibitor[Title/Abstract] OR docking[Title/Abstract] OR "binding affinity"[Title/Abstract]) AND antimalarial[Title/Abstract] AND "journal article"[pt] NOT "clinical trial"[pt] AND "free full text"[filter] AND 2015:2026[dp]
+""" 
+
 # Initial search to get the IDs
-stream = Entrez.esearch(db="pubmed", term="PfDHFR (Plasmodium falciparum dihydrofolate reductase)", retmax="10")
+stream = Entrez.esearch(db="pubmed", term=query, retmax="30")
 records = Entrez.read(stream)
 records_ids = records["IdList"]
 
+title = ["ID", "TITLE", "ABSTARCT"]
+
 # data to append
-info_to_append = []
+info_to_append = [title,]
 
 # Loop over IDs
 for id in records_ids:
@@ -40,7 +50,7 @@ for id in records_ids:
     info = [id, paper_title, abstract_content]
     info_to_append.append(info)
     
-    
+# Writing my search into a csv file : reults.csv
 file = open(data_path, "a", newline="", encoding="utf-8")
 writer = csv.writer(file)
 writer.writerows(info_to_append)
